37f55ab864ea416efd79f6e51f3a4555d43bca28
Yudi <rohmat.wahyudi@pintap.id>
2022-06-26 07:27:05 +0700

    challenge-09

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,11 +1,23 @@
 // Terima props lalu tampilkan dalam Contact component
 // Kalian bisa membuat CSS sendiri di src/components/Contact.css
 import React from 'react';
-
-const Contact = () => {
+import "./style.css";
+const Contact = (props) => {
     return (
-        <></>
-    )
+		<div className="card">
+			<img
+				src={props.data.photo}
+				alt={props.data.name}
+				className="img-size"
+			/>
+
+			<div className="content">
+				<h3>{props.data.name}</h3>
+				<p className="title">{props.data.email}</p>
+				<p>{props.data.phone}</p>
+			</div>
+		</div>
+	);
 }
 
 export default Contact;
--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,14 @@ import React from 'react';
 
 const Header = () => {
     return (
-        <></>
+        <>
+			<div>
+				<h2 style={{ marginBottom: "0" }}>Call a Friend</h2>
+				<p style={{ marginBottom: "10px" }}>
+					your friendly contact app
+				</p>
+			</div>
+		</>
     )
 }
 
